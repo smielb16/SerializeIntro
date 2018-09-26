aa2575f6822f51b6201137e7c83da491dcf1ec5a
Friedrich Gutmensch <smielb16@htlkaindorf.at>
2018-09-26 12:52:00 +0200

    implemented load in SchuelerBL

--- SerializeIntro/src/SchuelerBL.java
+++ SerializeIntro/src/SchuelerBL.java
@@ -1,19 +1,21 @@
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
-
 public class SchuelerBL {
-    
+
     private ArrayList<Schueler> klasse = new ArrayList<>();
-    
-    public void add(Schueler s){
+
+    public void add(Schueler s) {
         klasse.add(s);
     }
-    
-    public void save(File f) throws Exception{
+
+    public void save(File f) throws Exception {
         BufferedWriter bw = new BufferedWriter(
                 new FileWriter(f));
         for (Schueler s : klasse) {
@@ -23,9 +25,25 @@ public class SchuelerBL {
             bw.newLine();
         }
     }
-    
-    public void load(File f){
-        
+
+    public void load(File f) throws Exception {
+        BufferedReader br = new BufferedReader(
+                new FileReader(f));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] parts = line.split(";");
+            try {
+                add(new Schueler(parts[0],
+                        LocalDate.parse(parts[1])));
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
     }
-    
+
+    public static void main(String[] args) {
+
+    }
+
 }
