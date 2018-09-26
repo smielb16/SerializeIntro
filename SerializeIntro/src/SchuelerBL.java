88ceb565e1f01e15e543ef69e0a48b8d4bcddd00
Friedrich Gutmensch <smielb16@htlkaindorf.at>
2018-09-26 12:48:06 +0200

    implemented save in SchuelerBL

--- SerializeIntro/src/SchuelerBL.java
+++ SerializeIntro/src/SchuelerBL.java
@@ -1,5 +1,7 @@
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 
@@ -11,8 +13,15 @@ public class SchuelerBL {
         klasse.add(s);
     }
     
-    public void save(File f){
-        
+    public void save(File f) throws Exception{
+        BufferedWriter bw = new BufferedWriter(
+                new FileWriter(f));
+        for (Schueler s : klasse) {
+            bw.write(s.getName());
+            bw.write(";");
+            bw.write(s.getBirthday().toString());
+            bw.newLine();
+        }
     }
     
     public void load(File f){
