6d88bf100bc503e8ae9b6edadf9267e13a72a7df
Friedrich Gutmensch <smielb16@htlkaindorf.at>
2018-09-26 12:58:05 +0200

    added test code

--- SerializeIntro/src/SchuelerBL.java
+++ SerializeIntro/src/SchuelerBL.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 
 public class SchuelerBL {
@@ -24,6 +25,7 @@ public class SchuelerBL {
             bw.write(s.getBirthday().toString());
             bw.newLine();
         }
+        bw.flush();
     }
 
     public void load(File f) throws Exception {
@@ -43,7 +45,23 @@ public class SchuelerBL {
     }
 
     public static void main(String[] args) {
+        Schueler s1
+                = new Schueler("Hans",
+                        LocalDate.of(2000, Month.MARCH, 3));
+        Schueler s2
+                = new Schueler("Grete",
+                        LocalDate.of(2000, Month.OCTOBER, 10));
 
+        SchuelerBL bl = new SchuelerBL();
+        bl.add(s1);
+        bl.add(s2);
+
+        File f = new File("./data.csv");
+        try {
+            bl.save(f);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
